f43493c4b4373560ff613a05c33979658459750b
Maan Najjar <maan.najjar@gmail.com>
2015-12-16 12:13:55 -0500

    Renamed literals file and added makeRestId

--- LatteKit/src/io/lattekit/util/Util.java
+++ LatteKit/src/io/lattekit/util/Util.java
@@ -1,16 +1,16 @@
-package io.lattekit.xtend;
+package io.lattekit.util;
 
 import java.util.List;
 
 import org.eclipse.xtext.xbase.lib.Inline;
 
-public class ArrayLiterals2 {
+public class Util {
 	@Inline("new int[$1][]")
 	public static int[][] intArray(int outerSize) {
 		throw new UnsupportedOperationException();
 	}
 	
-	@Inline(value="io.lattekit.xtend.ArrayLiterals2.unwrap($1)", imported=ArrayLiterals2.class)
+	@Inline(value="io.lattekit.xtend.ArrayLiterals2.unwrap($1)", imported=Util.class)
 	public static int[][] unwrap(List<List<Integer>> list) {
 		int[][] arr = new int[list.size()][];
 		int i = 0;
@@ -20,6 +20,10 @@ public class ArrayLiterals2 {
 			for (int innerVal : inner) innerArr[j++] = innerVal;
 		}
 		return arr;
+	}
+	
+	public static int makeResId(String packageName, String type, String entryName) {
+    	return (((packageName.hashCode()+1)<<24) | (((type.hashCode()+1)&0xFF)<<16) | (entryName.hashCode()&0xFFFF));		
 	}
 
 }
