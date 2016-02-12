53413a45b69a93f0747ff99835976f141f58ee84
Maan Najjar <maan.najjar@gmail.com>
2016-02-12 01:55:53 -0500

    Added @Target to work in Kotlin. Changed name param to value

--- lattekit-core/src/main/java/io/lattekit/annotation/Prop.java
+++ lattekit-core/src/main/java/io/lattekit/annotation/Prop.java
@@ -1,12 +1,15 @@
 package io.lattekit.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by maan on 2/8/16.
  */
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Prop {
-    String name() default "";
+    String value() default "";
 }
