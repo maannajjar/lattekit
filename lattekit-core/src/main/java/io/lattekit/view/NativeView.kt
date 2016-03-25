d59041b126683e28776fea5322ae63c617359bc6
Maan Najjar <maan.najjar@gmail.com>
2016-03-24 20:45:17 -0400

    Set props during runtime when we couldn't find the class during compilation

--- lattekit-core/src/main/java/io/lattekit/view/NativeView.kt
+++ lattekit-core/src/main/java/io/lattekit/view/NativeView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.MotionEvent
 import android.view.View
 import android.widget.AdapterView
+import io.lattekit.Latte
+import java.lang.reflect.Method
 
 /**
  * Created by maan on 2/15/16.
@@ -15,43 +17,13 @@ open class NativeView : LatteView(), View.OnClickListener, View.OnTouchListener 
     var nativeViewClass : Class<out View>? = null
 
     var propsSetter : (NativeView, Map<String,Any?>)->List<String> = { view, props -> emptyList() };
+    val methodCache = mutableMapOf<String, Method?>()
 
     open fun getViewClass() : Class<out View> {
         return if (nativeViewClass != null) {
             nativeViewClass as Class<out View>
         } else {
             View::class.java
-        }
-    }
-
-    fun renderNative(context : Context) : View {
-        var style = this.props.get("style");
-        if (style != null && style is Int) {
-            return getViewClass().constructors.find{ it.parameterTypes.size == 3 }?.newInstance(context,null,style) as View
-        } else {
-            var constructor = getViewClass().constructors.getOrNull(0)
-            if (constructor != null) {
-                if (constructor.parameterTypes.size == 1) {
-                    return constructor.newInstance(context) as View
-                } else if (constructor.parameterTypes.size == 2) {
-                    return constructor.newInstance(context,null) as View
-                } else if (constructor.parameterTypes.size == 3) {
-                    return constructor.newInstance(context,null,0) as View
-                }
-            }
-            throw Exception("Couldn't find suitable constructor for custom view ${getViewClass().name}")
-        }
-    }
-
-    open fun applyProps(applyTo : Map<String,Any?>) {
-        applyProps(applyTo, false);
-    }
-
-    fun applyProps(propsToApply : Map<String,Any?>, onlyDelayed : Boolean ) {
-        if (androidView != null) {
-            androidView?.isClickable = false
-            propsSetter.invoke(this,propsToApply);
-            return;
         }
     }
 
@@ -108,5 +80,124 @@ open class NativeView : LatteView(), View.OnClickListener, View.OnTouchListener 
         }
 
     }
+
+    fun renderNative(context : Context) : View {
+        var style = this.props.get("style");
+        if (style != null && style is Int) {
+            return getViewClass().constructors.find{ it.parameterTypes.size == 3 }?.newInstance(context,null,style) as View
+        } else {
+            var constructor = getViewClass().constructors.getOrNull(0)
+            if (constructor != null) {
+                if (constructor.parameterTypes.size == 1) {
+                    return constructor.newInstance(context) as View
+                } else if (constructor.parameterTypes.size == 2) {
+                    return constructor.newInstance(context,null) as View
+                } else if (constructor.parameterTypes.size == 3) {
+                    return constructor.newInstance(context,null,0) as View
+                }
+            }
+            throw Exception("Couldn't find suitable constructor for custom view ${getViewClass().name}")
+        }
+    }
+
+    open fun applyProps(applyTo : Map<String,Any?>) {
+        applyProps(applyTo, false);
+    }
+
+    fun applyProps(propsToApply : Map<String,Any?>, onlyDelayed : Boolean ) {
+        if (androidView != null) {
+            androidView?.isClickable = false
+            var acceptedProps = propsSetter.invoke(this,propsToApply);
+            var remainingProps = propsToApply.filterKeys { !acceptedProps.contains(it) && it != "onTouch" && it != "onClick" };
+            if (!getViewClass().name.startsWith("android.")) {
+                setPropsRuntime(remainingProps)
+            }
+            return;
+        }
+    }
+
+    fun setPropsRuntime(propsToApply : Map<String,Any?>) {
+        val myCls = getViewClass()
+        propsToApply.forEach { entry ->
+            var it = entry.key
+                val value = propsToApply.get(it);
+                var field = if (it.startsWith("@")) {
+                    it.substring(1)
+                } else it;
+                var isFn =  value is Function0<*>
+                var setter = "set" + field.substring(0, 1).toUpperCase() + field.substring(1) + (if (isFn) "Listener" else "")
+                if (value == null) {
+                } else {
+                    var valueCls = value.javaClass
+
+                    var methodKey = myCls.toString() + ":" + field + ":" + value.javaClass
+                    var method = methodCache.get(methodKey);
+                    if (!methodCache.containsKey(methodKey)) {
+                        // This is the first time we look
+                        method = findSetter(setter, valueCls, isFn)
+                        if (method == null) {
+                            setter = field;
+                            method = findSetter(setter, valueCls, isFn)
+                        }
+                        methodCache.put(methodKey, method);
+                    }
+                    if (method != null) {
+                        method.invoke(androidView, if (isFn) Latte.createLambdaProxyInstance(method.parameterTypes.get(0), value as Object) else value);
+                    }
+
+            }
+        }
+    }
+
+    fun findSetter(setter : String , valueType: Class<Any>, isFnValue : Boolean) : Method? {
+        var myCls = getViewClass();
+        var currentCls : Class<*> = myCls;
+        var valueCls : Class<*> = valueType;
+        var reachedEnd = false;
+        var method : Method?;
+        while (!reachedEnd) {
+            if (isFnValue) {
+                method = currentCls.declaredMethods.find{ it.name == setter };
+                if (method != null && method.parameterTypes.size == 1) {
+                    var listenerInterface : Class<*> = method?.parameterTypes?.get(0)!!;
+                    if (listenerInterface.isInterface) {
+                        return method;
+                    }
+                } else {
+                    currentCls = currentCls.superclass
+                    if (currentCls == Object::class.java) {
+                        reachedEnd = true;
+                    }
+                }
+            } else {
+                try {
+                    method = myCls.getMethod(setter, valueCls);
+                    return method;
+                } catch(ex : NoSuchMethodException) {
+                }
+                try {
+                    var primitiveType = valueCls.getField("TYPE").get(null) as Class<*>;
+                    method = myCls.getMethod(setter, primitiveType);
+                    return method;
+                } catch(nsfe : NoSuchFieldException ) {
+                } catch(ex : NoSuchMethodException ) {
+                }
+                for(iface in valueCls.interfaces) {
+                    try {
+                        method = myCls.getMethod(setter, iface);
+                        return method;
+                    } catch(ex: NoSuchMethodException ) {
+                    }
+                }
+            }
+            if (valueCls.superclass == null) {
+                reachedEnd = true;
+            } else {
+                valueCls = valueCls.superclass
+            }
+        }
+        return null;
+    }
+
 }
 
