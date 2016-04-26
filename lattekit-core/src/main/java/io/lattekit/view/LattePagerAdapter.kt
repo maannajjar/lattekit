9dc9971ffe41c30037eac19e52a81ffcdced0dbf
Maan Najjar <maan.najjar@gmail.com>
2016-04-26 12:00:03 -0400

    Using FragmentStatePagerAdapter when dataset is supplied

---
+++ lattekit-core/src/main/java/io/lattekit/view/LattePagerAdapter.kt
@@ -0,0 +1,123 @@
+package io.lattekit.view
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
+import java.lang.reflect.ParameterizedType
+
+/**
+ * Created by maan on 4/26/16.
+ */
+
+class LattePagerAdapter(var parentView : LatteView) {
+    val data: List<*>?
+        get() {
+            val _get = parentView.props["data"]
+            return _get as List<*>?
+        }
+    fun isMatch(template: LatteView, item: Any, position: Int): Boolean {
+        var testLambda = template.props.get("when");
+        if (testLambda == null) {
+            return false;
+        }
+        if (testLambda !is kotlin.Function1<*, *> && testLambda !is kotlin.Function2<*, *, *>) {
+            LatteView.log("Latte", "Warning 'when' should be a lambda ")
+            return false;
+        }
+        var isFn2 = testLambda is kotlin.Function2<*, *, *>
+        var modelType = (testLambda.javaClass.genericInterfaces.get(0) as ParameterizedType).actualTypeArguments.get(0) as Class<*>
+
+        if (modelType.isAssignableFrom(item.javaClass)) {
+            var secondParamType: Class<*>? = null
+            if (isFn2) {
+                secondParamType = (testLambda.javaClass.genericInterfaces.get(0) as ParameterizedType).actualTypeArguments.get(1) as Class<*>;
+                if (!secondParamType.isAssignableFrom(Integer::class.java)) {
+                    LatteView.log("Warning: second parameter's type is " + secondParamType.name + ". It must be an integer which will contain model index")
+                    return false;
+                }
+            }
+            var isMatch = if (!isFn2) {
+                (testLambda as Function1<Any,Any>).invoke(item) as Boolean
+            } else {
+                (testLambda as Function2<Any,Any,Any>).invoke(item,position) as Boolean
+            }
+            return isMatch;
+        } else {
+            LatteView.log("Warning: model of type " + item.javaClass.name + " is not assignable to " + modelType)
+        }
+        return false;
+    }
+
+
+    fun getCount(): Int {
+        return data?.size ?: parentView.children.size
+    }
+
+    fun getPageTitle(position: Int): CharSequence? {
+        var template = getMatchingTemplate(position);
+        var pageTitle = template.props.get("pageTitle")
+        if (pageTitle != null && pageTitle is String) {
+            return pageTitle;
+        }
+        return null;
+    }
+
+    fun getMatchingTemplate(position: Int) : LatteView {
+        if (data != null) {
+            val item = data!![position]
+            var defaultView = -1
+            var selectedTemplate = -1
+            for (i in 0..parentView.children.size - 1) {
+                val child = parentView.children[i]
+                if (isMatch(child, item!!, position)!!) {
+                    selectedTemplate = i
+                }
+                if (child.props["defaultView"] == true || child.props["defaultView"] == "true") {
+                    defaultView = i
+                }
+            }
+            if (defaultView == -1 && selectedTemplate == -1) {
+                throw Exception("Couldn't find template for psoition " + Integer.valueOf(position)!!)
+            } else {
+                if (selectedTemplate == -1) {
+                    selectedTemplate = defaultView
+                }
+            }
+            val template = parentView.children[selectedTemplate]
+            return template
+        } else {
+            return parentView.children[position]
+        }
+    }
+
+    fun getItem(position: Int): Fragment {
+        var selectedTemplate = getMatchingTemplate(position);
+        if (data != null) {
+            // When data set is provided, the template will be shared by multiple items. So we should make a copy of it
+            val item = data!![position]
+            val template = selectedTemplate.copy()
+            template.props.put("modelIndex", Integer.valueOf(position))
+            template.props.put("model", item)
+            template.parentView = parentView
+            return LatteViewPager.PagerFragment.Companion.newInstance(template)
+        } else {
+            selectedTemplate.parentView = parentView
+            return LatteViewPager.PagerFragment.Companion.newInstance(selectedTemplate)
+        }
+    }
+}
+
+class LatteFragmentPagerAdapter(var latteView : LatteView) : FragmentPagerAdapter((latteView.activity!! as FragmentActivity).supportFragmentManager) {
+    var pagerAdapter = LattePagerAdapter(latteView);
+    override fun getCount(): Int = pagerAdapter.getCount()
+    override fun getItem(position: Int): Fragment? = pagerAdapter.getItem(position)
+    override fun getPageTitle(position: Int) = pagerAdapter.getPageTitle(position) ?: super.getPageTitle(position)
+}
+
+class LatteFragmentStatePagerAdapter(var latteView : LatteView) : FragmentStatePagerAdapter((latteView.activity!! as FragmentActivity).supportFragmentManager) {
+    var pagerAdapter = LattePagerAdapter(latteView);
+    override fun getCount(): Int = pagerAdapter.getCount()
+    override fun getItem(position: Int): Fragment? = pagerAdapter.getItem(position)
+    override fun getPageTitle(position: Int) = pagerAdapter.getPageTitle(position) ?: super.getPageTitle(position)
+}
--- lattekit-core/src/main/java/io/lattekit/view/LatteViewPager.kt
+++ lattekit-core/src/main/java/io/lattekit/view/LatteViewPager.kt
@@ -2,21 +2,13 @@ package io.lattekit.view
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import io.lattekit.util.Util
-import java.lang.reflect.ParameterizedType
 import java.util.*
-
-/**
- * Created by maan on 3/8/16.
- */
-
-
 
 /**
  * Created by maan on 2/7/16.
@@ -55,98 +47,26 @@ class LatteViewPager : NativeView() {
         }
     }
 
-    private var adapter: FragmentPagerAdapter? = null
-
-    val data: List<*>?
-        get() {
-            val _get = this.props["data"]
-            return _get as List<*>?
-        }
-
+    private var adapter: PagerAdapter? = null
 
     override fun getViewClass(): Class<out View> {
         return android.support.v4.view.ViewPager::class.java
     }
 
     override fun onViewCreated() {
-        this.androidView?.id = Util.makeResId("io.lattekit", "id", "viewPager")
+        if (!this.props.containsKey("id")) {
+            this.androidView?.id = Util.makeResId("io.lattekit", "id", "viewPager")
+        }
 
-        this.adapter = object : FragmentPagerAdapter((this.activity as FragmentActivity).supportFragmentManager) {
-
-            override fun getCount(): Int {
-                return data?.size ?: children.size
-            }
-
-            override fun getItem(position: Int): Fragment {
-                if (data != null) {
-                    val item = data!![position]
-                    var defaultView = -1
-                    var selectedTemplate = -1
-                    for (i in 0..children.size - 1) {
-                        val child = children[i]
-                        if (isMatch(child, item!!, position)!!) {
-                            selectedTemplate = i
-                        }
-                        if (child.props["defaultView"] == true || child.props["defaultView"] == "true") {
-                            defaultView = i
-                        }
-                    }
-                    if (defaultView == -1 && selectedTemplate == -1) {
-                        throw Exception("Couldn't find template for psoition " + Integer.valueOf(position)!!)
-                    } else {
-                        if (selectedTemplate == -1) {
-                            selectedTemplate = defaultView
-                        }
-                    }
-                    val template = children[selectedTemplate].copy()
-                    template.props.put("modelIndex", Integer.valueOf(position))
-                    template.props.put("model", item)
-                    template.parentView = this@LatteViewPager
-                    return LatteViewPager.PagerFragment.Companion.newInstance(template)
-                } else {
-                    var subView = children[position]
-                    subView.parentView = this@LatteViewPager
-                    return LatteViewPager.PagerFragment.Companion.newInstance(subView)
-                }
-            }
+        if (this.props.containsKey("data")) {
+            this.adapter = LatteFragmentStatePagerAdapter(this);
+        } else {
+            this.adapter = LatteFragmentPagerAdapter(this);
         }
         val view = this.androidView as android.support.v4.view.ViewPager
         view.adapter = this.adapter
         this.adapter!!.notifyDataSetChanged()
         super.onViewCreated()
-    }
-
-    fun isMatch(template: LatteView, item: Any, position: Int): Boolean {
-        var testLambda = template.props.get("when");
-        if (testLambda == null) {
-            return false;
-        }
-        if (testLambda !is kotlin.Function1<*, *> && testLambda !is kotlin.Function2<*, *, *>) {
-            log("Latte", "Warning 'when' should be a lambda ")
-            return false;
-        }
-        var isFn2 = testLambda is kotlin.Function2<*, *, *>
-        var modelType = (testLambda.javaClass.genericInterfaces.get(0) as ParameterizedType).actualTypeArguments.get(0) as Class<*>
-
-        if (modelType.isAssignableFrom(item.javaClass)) {
-            var secondParamType: Class<*>? = null
-            if (isFn2) {
-                secondParamType = (testLambda.javaClass.genericInterfaces.get(0) as ParameterizedType).actualTypeArguments.get(1) as Class<*>;
-                if (!secondParamType.isAssignableFrom(Integer::class.java)) {
-                    LatteView.log("Warning: second parameter's type is " + secondParamType.name + ". It must be an integer which will contain model index")
-                    return false;
-                }
-            }
-            var isMatch = if (!isFn2) {
-                (testLambda as Function1<Any,Any>).invoke(item) as Boolean
-            } else {
-                (testLambda as Function2<Any,Any,Any>).invoke(item,position) as Boolean
-            }
-            return isMatch;
-        } else {
-            LatteView.log("Warning: model of type " + item.javaClass.name + " is not assignable to " + modelType)
-        }
-        return false;
     }
 
 }
