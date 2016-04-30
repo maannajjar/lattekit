4b1a90fb224e229e26f70b2f50c30ebccbc66913
Maan Najjar <maan.najjar@gmail.com>
2016-04-29 23:37:07 -0400

    Moving SourceBuilder to its own file

--- gradle-plugin/src/main/java/io/lattekit/template/DSL.kt
+++ gradle-plugin/src/main/java/io/lattekit/template/DSL.kt
@@ -6,31 +6,6 @@ import kotlin.reflect.KClass
  * Created by maan on 4/28/16.
  */
 
-open class SourceBuilder() {
-    var currentIndent = 0;
-    var lastIndent = 0;
-
-    var buffer = StringBuffer();
-
-    fun append(line : String,newLine : Boolean = true,addIndent : Boolean = true) {
-        if (addIndent) for (i in 0..currentIndent-1) buffer.append("*")
-        buffer.append(line);
-        if (newLine) buffer.append("\r\n");
-    }
-    fun indent(indent : Int = 4) {
-        lastIndent = indent;
-        currentIndent += indent;
-    }
-    fun unindent() {
-        currentIndent -= lastIndent;
-    }
-
-    override fun toString(): String {
-        return buffer.toString()
-    }
-
-}
-
 open class KotlinElement( var parent : KotlinElement?) {
 
     var children = mutableListOf<KotlinElement>()
