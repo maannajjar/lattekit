bcc1e4e759c5be62db30b6398de63555c5b24726
Maan Najjar <maan.najjar@gmail.com>
2017-02-14 12:02:24 -0500

    Changed to appcompat

--- lattekit-core/src/main/java/io/lattekit/activity/LatteActivity.kt
+++ lattekit-core/src/main/java/io/lattekit/activity/LatteActivity.kt
@@ -2,8 +2,7 @@ package io.lattekit.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.FrameLayout
 import io.lattekit.Latte
@@ -14,7 +13,7 @@ import io.lattekit.view.LatteView
 /**
  * Created by maan on 2/15/16.
  */
-open class LatteActivity : FragmentActivity()  {
+open class LatteActivity : AppCompatActivity()  {
 
     var latteView : LatteView? = null;
     var androidView  : View? = null;
